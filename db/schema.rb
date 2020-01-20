bb18d5b7fc8bbc3a6263201b66b4d4b37d24d6df
Marvin Laucher <mlaucher@zipcar.com>
2020-01-20 22:29:47 +0000

    update to use pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_20_182221) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
